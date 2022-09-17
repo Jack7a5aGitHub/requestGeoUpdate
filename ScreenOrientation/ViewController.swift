8a233b46d5da7b6d53e35b4a54c20ff783c46ad9
Jack7a5aGitHub <jack7a5a1993@icloud.com>
2022-09-18 01:00:26 +0900

    init

---
+++ ScreenOrientation/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  ScreenOrientation
+//
+//  Created by Jack Wong on 2022/09/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override var preferredInterfaceOrientationForPresentation: UIInterfaceOrientation {
+        return .portrait
+    }
+    
+    
+    @IBAction func fullScreenToggle(_ sender: UIButton) {
+        
+        if #available(iOS 16, *) {
+            DispatchQueue.main.async {
+                let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+                let orientation = windowScene?.interfaceOrientation
+                self.setNeedsUpdateOfSupportedInterfaceOrientations()
+                self.navigationController?.setNeedsUpdateOfSupportedInterfaceOrientations()
+                windowScene?.requestGeometryUpdate(.iOS(interfaceOrientations: orientation == .portrait ? .landscape : .portrait)) { error in
+                    print(error)
+                    print(windowScene?.effectiveGeometry ?? "")
+                }
+            }
+        }
+    }
+    
+}
+
